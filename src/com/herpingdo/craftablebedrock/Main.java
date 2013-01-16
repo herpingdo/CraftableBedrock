c7bdcce6cd66bfd74e3907b2dedfb36b215aa668
Ryan K <derp@derpy.fr>
2013-01-16 01:48:57 -0500

    First commit

---
+++ src/com/herpingdo/craftablebedrock/Main.java
@@ -0,0 +1,17 @@
+package com.herpingdo.craftablebedrock;
+
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	private ShapedRecipe bedrock = new ShapedRecipe(new ItemStack(Material.BEDROCK, 2)).shape("ooo", "odo", "ooo").setIngredient('o', Material.OBSIDIAN).setIngredient('d', Material.DIAMOND);
+	
+	@Override
+	public void onEnable()
+	{
+		getServer().addRecipe(bedrock);
+		//Stuff
+	}
+}
